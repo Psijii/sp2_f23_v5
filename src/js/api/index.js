9b31466c457158f8bdc0ffdc7edb050aa500ba57
Siw Elin Årolilja Iversen <111748314+Psijii@users.noreply.github.com>
2023-12-15 10:59:59 +0100

    More testing

--- src/js/api/index.js
+++ src/js/api/index.js
@@ -1,10 +1,10 @@
-export * from './apiCall.mjs';
-export * from './fetchListings.mjs';
-export * from './opts.mjs';
-export * from './setBid.mjs';
-export * from './fetchUser.mjs';
-export * from './updProfile.mjs';
-export * from './createListing.mjs';
-export * from './delListing.mjs';
+export * from "./apiCall.mjs";
+export * from "./fetchListings.mjs";
+export * from "./opts.mjs";
+export * from "./setBid.mjs";
+export * from "./fetchUser.mjs";
+export * from "./updProfile.mjs";
+export * from "./createListing.mjs";
+export * from "./delListing.mjs";
 
 //load file to easier handle exports and imports etc.
